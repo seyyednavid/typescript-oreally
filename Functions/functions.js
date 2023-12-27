9a2105a1fee83b644f03e5d89eb7c111fabc12ec
seyyednavid hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-12-27 20:10:43 +0000

    Working on functions in typescript

---
+++ Functions/functions.js
@@ -0,0 +1,51 @@
+function hello(name) {
+    return "Hello" + " " + name;
+}
+// console.log(hello("navid"));
+function add(num1, num2) {
+    return num1 + num2;
+}
+// console.log("Sum is: ",add(10,20));
+function display(id, name, role) {
+    console.log("Name: ", name);
+    console.log("ID: ", id);
+    console.log("Role: ", role);
+}
+// console.log(display(1, "Navid", "Admin"));
+// optional parameter ==> role
+function display2(id, name, role) {
+    console.log("Name: ", name);
+    console.log("ID: ", id);
+    if (role != undefined) {
+        console.log("Role: ", role);
+    }
+}
+// console.log(display2(1, "Navid"));
+//Default value
+function display3(id, name, role) {
+    if (role === void 0) { role = "Normal"; }
+    console.log("Name: ", name);
+    console.log("ID: ", id);
+    //   if (role != undefined) {
+    console.log("Role: ", role);
+    //   }
+}
+// console.log(display3(1, "Navid"));
+// console.log(display3(1, "Navid", "Admin"));
+//pass function as argument
+function calculator(fun) {
+    console.log(fun(10, 20));
+}
+// calculator(add)
+//Return function
+function calculator2() {
+    function subtract(num1, num2) {
+        return num1 - num2;
+    }
+    return subtract;
+}
+// Get subtract function under the name of sub
+var sub = calculator2();
+// console.log(sub);
+console.log(sub(20, 5));
+console.log(calculator2()(20, 5));
---
+++ Functions/functions.ts
@@ -0,0 +1,63 @@
+function hello(name: string): string {
+  return "Hello" + " " + name;
+}
+// console.log(hello("navid"));
+
+
+function add(num1: number, num2: number): number {
+  return num1 + num2;
+}
+// console.log("Sum is: ",add(10,20));
+
+
+function display(id: number, name: string, role: string) {
+  console.log("Name: ", name);
+  console.log("ID: ", id);
+  console.log("Role: ", role);
+}
+// console.log(display(1, "Navid", "Admin"));
+
+
+// optional parameter ==> role
+function display2(id: number, name: string, role?: string) {
+  console.log("Name: ", name);
+  console.log("ID: ", id);
+  if (role != undefined) {
+    console.log("Role: ", role);
+  }
+}
+// console.log(display2(1, "Navid"));
+
+
+//Default value
+function display3(id: number, name: string, role: string = "Normal") {
+  console.log("Name: ", name);
+  console.log("ID: ", id);
+  //   if (role != undefined) {
+  console.log("Role: ", role);
+  //   }
+}
+// console.log(display3(1, "Navid"));
+// console.log(display3(1, "Navid", "Admin"));
+
+
+//pass function as argument
+function calculator(fun: any): void {
+  console.log(fun(10, 20));
+}
+// calculator(add)
+
+
+//Return function
+function calculator2(): any {
+  function subtract(num1: number, num2: number): number {
+    return num1 - num2;
+  }
+  return subtract;
+}
+// Get subtract function under the name of sub
+var sub = calculator2();
+// console.log(sub);
+console.log(sub(20,5));
+//Shortcut way
+console.log(calculator2()(20,5));
