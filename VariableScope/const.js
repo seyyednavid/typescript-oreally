5d6d59041e38de468b4f628957a329f83255a0d6
seyyednavid hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-12-29 12:19:30 +0000

    work on variable scope

---
+++ VariableScope/const.js
@@ -0,0 +1,9 @@
+var pi = 3.14;
+// console.log(pi);
+var product = function (x, y) {
+    return x * y;
+};
+// product = function (x: number, y: number): number {
+//   return x + y;
+// };
+console.log(product(2, 5));
---
+++ VariableScope/const.ts
@@ -0,0 +1,11 @@
+const pi = 3.14;
+// console.log(pi);
+
+const product = function (x: number, y: number): number {
+  return x * y;
+};
+// product = function (x: number, y: number): number {
+//   return x + y;
+// };
+
+console.log(product(2, 5));
